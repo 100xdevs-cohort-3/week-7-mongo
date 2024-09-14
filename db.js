9b5d1377f0f285229f8f36d12fb94d25c70b912a
AnujChhikara <anujchhikara777@gmail.com>
2024-09-14 21:41:41 +0530

    improvements

--- db.js
+++ db.js
@@ -1,19 +1,68 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const User = new Schema({
-  name: String,
-  email: {type: String, unique: true},
-  password: String
-});
+
+const User= new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Email is required'],
+    lowercase: true,  // Ensures email is stored in lowercase
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],  // RegExp for email validation
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters'],  // Minimum length
+    validate: {
+      validator: function(v) {
+        return /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).{6,}$/.test(v);
+      },
+      message: 'Password must contain at least one letter, one number, and one special character',
+    }
+  }
+}, { timestamps: true });
+
 
 const Todo = new Schema({
-    userId: ObjectId,
+    userId:ObjectId,
     title: String,
-    done: Boolean
+    done:{
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now },
+
+    dueDate: {
+      type: Date,
+      required: false  
+    }
+},{timestamps:true});
+
+User.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
+
+
+User.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
 
 const UserModel = mongoose.model('users', User);
 const TodoModel = mongoose.model('todos', Todo);
--- index.js
+++ index.js
@@ -4,25 +4,60 @@ const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
-mongoose.connect("")
 
+const port  = 5000;
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
+app.post("/signup", async function(req, res) {
+  const { email, password, name } = req.body;
+
+  if (!email || !password || !name) {
+    return res.status(400).json({
+      message: "Please provide all the details"
     });
+  }
+
+  try {
+
+    const existingUser = await UserModel.findOne({ email });
     
-    res.json({
-        message: "You are signed up"
-    })
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    const newUser = await UserModel.create({
+      email,
+      password,
+      name
+    });
+
+
+    res.status(201).json({
+      message: "You are signed up",
+      userId: newUser._id 
+    });
+  } catch (error) {
+    // Catch any unexpected errors
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({
+        message: 'Validation error',
+        errors: Object.keys(error.errors).map(key => ({
+          field: key,
+          message: error.errors[key].message
+        }))
+      });
+    }
+
+ 
+    res.status(500).json({
+      message: "An internal server error occurred",
+      error: error.message
+    });
+  }
 });
 
 
@@ -30,14 +65,21 @@ app.post("/signin", async function(req, res) {
     const email = req.body.email;
     const password = req.body.password;
 
-    const response = await UserModel.findOne({
-        email: email,
-        password: password,
+    const user = await UserModel.findOne({
+        email
     });
+
+    if (!user) {
+        return res.status(403).json({
+            message: "Incorrect creds"
+        })
+    } 
+
+    const response = user.isPasswordCorrect(password);
 
     if (response) {
         const token = jwt.sign({
-            id: response._id.toString()
+            id: user._id.toString()
         }, JWT_SECRET);
 
         res.json({
@@ -55,11 +97,19 @@ app.post("/todo", auth, async function(req, res) {
     const userId = req.userId;
     const title = req.body.title;
     const done = req.body.done;
+    const dueDate = req.body.dueDate ? new Date(req.body.dueDate) : null;
+
+    if(!title) {
+        return res.status(400).json({
+            message: "Please provide title"
+        })
+    }
 
     await TodoModel.create({
         userId,
         title,
-        done
+        done,
+        dueDate
     });
 
     res.json({
@@ -80,4 +130,44 @@ app.get("/todos", auth, async function(req, res) {
     })
 });
 
-app.listen(3000);
+app.post("/todoFinish/:todoId", auth, async function(req, res) {
+    const {todoId }= req.params
+    const userId = req.userId;
+
+    const todo = await TodoModel.findOne({
+        _id: todoId,
+        userId
+    });
+
+    if (!todo) {
+        return res.status(404).json({
+            message: "Todo not found"
+        })
+    }
+
+   const updatedTodo = await TodoModel.findOneAndUpdate({
+        _id: todoId,
+        userId
+    }, {
+        done: true
+    });
+    if (!updatedTodo) {
+        return res.status(404).json({
+            message: "Todo not found"
+        })
+    } 
+
+    res.json({
+        message: "Todo marked as done"
+    })
+
+});
+
+mongoose.connect("/paste mongobd URI").then(
+    app.listen(port, () => {
+        console.log(`Server is running at ${port}`);
+    })
+)
+.catch((err) => {
+    console.log("MONGO db connection failed", err);
+})
