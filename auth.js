ab8877b7a984df1af8c8960c8c0d1a13c1146f3e
Kirat <harkirat96@gmail.com>
2024-09-15 19:42:43 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ function auth(req, res, next) {
     const response = jwt.verify(token, JWT_SECRET);
 
     if (response) {
-        req.userId = token.id;
+        req.userId = response.id;
         next();
     } else {
         res.status(403).json({
